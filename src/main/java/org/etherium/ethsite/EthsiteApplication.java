eaf800f30fce508bba94cf9ec35ec2673e7b349a
IsMoreiraKt <ismaelmoreiraubuntu@gmail.com>
2024-11-23 07:59:15 +0000

    style: Modification of the input file to pass the checkstyle.

--- src/main/java/org/etherium/ethsite/EthsiteApplication.java
+++ src/main/java/org/etherium/ethsite/EthsiteApplication.java
@@ -3,11 +3,25 @@ package org.etherium.ethsite;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main entry point for the Ethsite application.
+ */
 @SpringBootApplication
-public class EthsiteApplication {
+public final class EthsiteApplication {
+    private EthsiteApplication() {
+        throw new UnsupportedOperationException(
+            "Utility class should not be instantiated"
+        );
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(EthsiteApplication.class, args);
-	}
-
+    /**
+     * Main entry point for the Ethsite application.
+     * @param args Command-line arguments
+     */
+    public static void main(final String[] args) {
+        SpringApplication.run(
+            EthsiteApplication.class,
+            args
+        );
+    }
 }
