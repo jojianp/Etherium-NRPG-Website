13df961eb87c233544f53af356b10bb35a7dc18e
IsMoreiraKt <ismaelmoreiraubuntu@gmail.com>
2024-11-23 04:28:34 +0000

    feat: Initial commit.

---
+++ src/main/java/org/etherium/ethsite/EthsiteApplication.java
@@ -0,0 +1,13 @@
+package org.etherium.ethsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EthsiteApplication.class, args);
+	}
+
+}
